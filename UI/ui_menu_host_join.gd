d3d4b3dc4dcc6c551ada5d57db2001fee153fbdb
2Rman <67239506+2Rman@users.noreply.github.com>
2025-06-16 23:56:42 +0300

    Corrections
    added buttons into lobby screen, fixed reaction of button "return", all button alligned, added button into host-join menu - "return".

--- UI/ui_menu_host_join.gd
+++ UI/ui_menu_host_join.gd
@@ -5,6 +5,8 @@ extends Control
 func _on_button_host_pressed() -> void:
 	NetworkManager.host()
 
-
 func _on_button_connect_pressed() -> void:
 	NetworkManager.join(ip_address.text)
+
+func _on_button_return_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/ui_menu.tscn")
